f63d4f4c7cb6e1f71cdad9b5ea74dcafba390502
DESKTOP-TI42AN4\long.nn <long.nn.aris-vn.com@outlook.com>
2022-12-12 17:55:58 +0700

    fixed cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ const app = express()
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+app.use(cors())
 
 app.use((req, res, next) => {
     //** Log the request */
